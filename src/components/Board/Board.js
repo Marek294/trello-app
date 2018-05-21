9b43fc39e4b1bf91e196e72b54b2b59a10715e65
Marek294 <Markra5835@gmail.com>
2018-05-21 09:39:51 +0200

    move functions and state to Boards Component, initialize drag and drop tasks to other board

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -3,67 +3,22 @@ import Task from '../Task/Task';
 import './Board.css';
 
 class Board extends Component {
-    state = {
-        tasks: [
-            {
-                id: 1,
-                text: 'Pierwsze zadanie'
-            },
-            {
-                id: 2,
-                text: 'Drugie zadanie'
-            },
-            {
-                id: 3,
-                text: 'Trzecie zadanie'
-            }
-        ],
-        draggedId: null
-    }
-
-    handleOnTaskDragStart = id => e => {
-        this.setState({
-            draggedId: id
-        })
-    }
-
-    handleOnTaskDrop = id => e => {
-        e.preventDefault();
-        const { tasks, draggedId } = this.state
-
-        const draggedIndex = tasks.findIndex(item => item.id === draggedId)
-        const dropedIndex = tasks.findIndex(item => item.id === id)
-        const element = tasks[draggedIndex]
-
-        let newTasks = [
-        ...tasks.slice(0, draggedIndex),
-        ...tasks.slice(draggedIndex + 1)
-        ]
-
-        newTasks.splice(dropedIndex, 0, element)
-
-        this.setState({
-            tasks: newTasks
-        })
-
-    }
-
-    // onDragOverBoard = e => e.preventDefault();
+    onDragOverBoard = e => e.preventDefault();
 
     render() {
-        // const { onBoardDrop, id } = this.props;
-        const { tasks } = this.state;
+        const { onBoardDrop, tasks, handleOnTaskDragStart, handleOnTaskDrop } = this.props;
+
         const displayTasks = tasks.map((item,i) => {
             return (
                 <Task key={i}
                     item={item} 
-                    handleOnTaskDragStart={this.handleOnTaskDragStart}
-                    handleOnTaskDrop={this.handleOnTaskDrop} />
+                    handleOnTaskDragStart={handleOnTaskDragStart}
+                    handleOnTaskDrop={handleOnTaskDrop} />
             );
         })
 
         return (
-            <div className='board' >
+            <div className='board' onDragOver={this.onDragOverBoard} onDrop={onBoardDrop} >
                 {displayTasks} 
             </div>
         );
--- src/components/Boards/Boards.js
+++ src/components/Boards/Boards.js
@@ -2,17 +2,81 @@ import React, { Component } from 'react';
 import Board from '../Board/Board';
 
 class Boards extends Component {
-    // onBoardDrop = id => e => {
-    //     e.preventDefault();
-    //     console.log('board drop', id)
-    // }
+    state = {
+        tasks: [
+            {
+                id: 1,
+                board: 1,
+                text: 'Pierwsze zadanie'
+            },
+            {
+                id: 2,
+                board: 1,
+                text: 'Drugie zadanie'
+            },
+            {
+                id: 3,
+                board: 1,
+                text: 'Trzecie zadanie'
+            },
+            {
+                id: 4,
+                board: 2,
+                text: 'Czwarte zadanie'
+            }
+        ],
+        draggedId: null
+    }
+
+    handleOnTaskDragStart = id => e => {
+        this.setState({
+            draggedId: id
+        })
+    }
+
+    handleOnTaskDrop = id => e => {
+        console.log('task drop', id);
+        e.preventDefault();
+        const { tasks, draggedId } = this.state
+
+        const draggedIndex = tasks.findIndex(item => item.id === draggedId)
+        const dropedIndex = tasks.findIndex(item => item.id === id)
+        const element = tasks[draggedIndex]
+
+        let newTasks = [
+        ...tasks.slice(0, draggedIndex),
+        ...tasks.slice(draggedIndex + 1)
+        ]
+
+        newTasks.splice(dropedIndex, 0, element)
+
+        this.setState({
+            tasks: newTasks
+        })
+
+    }
+
+    onBoardDrop = id => e => {
+        e.preventDefault();
+        console.log('board drop', id)
+    }
+
+    filterTasks = boardId => {
+        const { tasks } = this.state;
+
+        return tasks.filter(item => item.board === boardId);
+    }
 
     render() {
         return (
             <div className='boards'>
                 <h1 className='boards__title'>Trello App</h1>
-                <Board />
-                {/* <Board onBoardDrop={this.onBoardDrop} id={2} /> */}
+                {[1, 2].map(id =>
+                    <Board id={id}
+                        handleOnTaskDragStart={this.handleOnTaskDragStart}
+                        handleOnTaskDrop={this.handleOnTaskDrop}
+                        onBoardDrop={this.onBoardDrop(id)}
+                        tasks={this.filterTasks(id)} />)}
             </div>
         );
     }
